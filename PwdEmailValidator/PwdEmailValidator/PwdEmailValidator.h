cce36243ba62b646b304922e1345d28e9233baf0
rashmikapde <rashmikapde@gmail.com>
2021-10-11 10:16:38 +0530

    add framework

---
+++ PwdEmailValidator/PwdEmailValidator/PwdEmailValidator.h
@@ -0,0 +1,18 @@
+//
+//  PwdEmailValidator.h
+//  PwdEmailValidator
+//
+//  Created by Treel on 11/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PwdEmailValidator.
+FOUNDATION_EXPORT double PwdEmailValidatorVersionNumber;
+
+//! Project version string for PwdEmailValidator.
+FOUNDATION_EXPORT const unsigned char PwdEmailValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PwdEmailValidator/PublicHeader.h>
+
+
